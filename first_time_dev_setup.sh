fb86edd4783d643b0ae4dc2278ce502bc71ee916
jonathan <jonathan@kyuss.org>
2014-09-25 04:45:54 -0700

    update first time setup for new project structure

--- first_time_dev_setup.sh
+++ first_time_dev_setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
+cd $(cd $(dirname $0) && pwd)
 git submodule init
 git submodule update
-cd cobbzilla-parent
+
+pushd utils
+git submodule init
+git submodule update
+popd
+
+pushd utils/cobbzilla-parent
 mvn install
-cd ..
+popd
