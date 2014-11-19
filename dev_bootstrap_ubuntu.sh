3ed1da24fd0838baf90aa9f4a844fe9e93388c57
jonathan <jonathan@kyuss.org>
2014-11-19 04:05:42 -0800

    fix command ordering

--- dev_bootstrap_ubuntu.sh
+++ dev_bootstrap_ubuntu.sh
@@ -15,10 +15,10 @@ apt-get install -y memcached redis-server postgresql daemon unzip
 # Create dev/test databases and users, set passwords
 for user in $(whoami) cloudos cloudos_dns wizard_form ; do
   sudo -u postgres -H createuser --createdb ${name}
-  sudo -u postgres -H bash -c "echo \"alter user ${user} with password '${user}'\" | psql -U postgres"
   for name in ${user} ${user}_test ; do
     sudo -u postgres -H createdb ${name}
   done
+  sudo -u postgres -H bash -c "echo \"alter user ${user} with password '${user}'\" | psql -U postgres"
 done
 
 # Set passwords for db users (needed to generate schemas).
